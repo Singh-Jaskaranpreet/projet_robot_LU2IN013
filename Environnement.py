c7062f7a2bf86ee2abd692fd6a8e62ab1d0231c0
Tsiory-Rama <dtsiorysteven@gmail.com>
2025-01-26 11:24:50 +0100

    Fusion des codes avec un obstacle

--- Environnement.py
+++ Environnement.py
@@ -19,11 +19,24 @@ class Environnement:
         elif (vehicule.r_Avg[1] - 10 < 0 or vehicule.r_Avd[1] - 10 < 0 or vehicule.r_Ar[1] - 10 < 0 ) :  # 25 est le rayon du cercle
             vehicule.arret()
 
-    def afficher(self, screen, vehicule, couleur_vehicule, couleur_texte):
-        # Points du triangle
-        
+    def afficher(self, screen, vehicule, couleur_vehicule, couleur_texte, objects):
         # Afficher le véhicule
-        pygame.draw.polygon(screen, couleur_vehicule,[point_Rarr,point_Rgauch,point_Rdroite])
+        rb=[[vehicule.r_Ar,vehicule.r_Avg,vehicule.r_Avd]]
+                # Afficher le véhicule
+        for v in rb:
+            voi=pygame.draw.polygon(screen, couleur_vehicule,v)
+        #pygame.draw.polygon(screen, couleur_vehicule,[vehicule.r_Ar,vehicule.r_Avg,vehicule.r_Avd])
+
+         # Création de l'obstacle numéro 1
+        
+        for obj in objects:
+            pygame.draw.rect(screen, (0,0,0), obj)
+        if voi.colliderect(obj):
+            if vehicule.vitesse > 0:
+                print('il y a un choc')
+            vehicule.arret()
+            vehicule.vitesse = 0
+            
 
         # Afficher la vitesse
         font = pygame.font.SysFont(None, 36)
