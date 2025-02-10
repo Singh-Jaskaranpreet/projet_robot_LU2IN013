4914954849a48d3d9ac335f790995fcca5c6a485
B1gPanda <kankizilmikail.gmail.com>
2025-02-10 15:48:15 +0100

    changement controle

---
+++ controleur.py
@@ -0,0 +1,36 @@
+import pygame
+import environnement
+import sys
+
+class Controleur:
+    def __init__(self, vehicule, environnement):
+        self.vehicule = vehicule
+        self.environnement = environnement
+
+    def gerer_evenements(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:  # Augmenter la vitesse de la roue droite
+            self.vehicule.vit_Rd = min(150, self.vehicule.vit_Rd + 1)
+         
+        elif keys[pygame.K_UP]:  # Ralentir la roue droite
+            self.vehicule.vit_Rd = max(-50, self.vehicule.vit_Rd - 1)
+
+        if keys[pygame.K_LEFT]:  # Augmenter la vitesse de la roue gauche
+            self.vehicule.vit_Rg = min(150, self.vehicule.vit_Rg + 1)
+
+        elif keys[pygame.K_DOWN]:  # Ralentir la roue gauche
+            self.vehicule.vit_Rg = max(-50, self.vehicule.vit_Rg - 1)
+
+        elif keys[pygame.K_f]:
+            self.vehicule.freiner(0.5)
+            print("                                                       ", end ="\r")
+            print("le vehicule freine, Pschhh", end ="\r")
+
+        if keys[pygame.K_r]:
+            self.vehicule.restart()
+            print("                                                       ", end ="\r")
+            print("oh la la on retourne à zero", end ="\r")
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import pygame
 import sys
-from vehicule import *
 from environnement import *
-from random import *
 from horloge import *
 from affichage import *
 import time
-import math
+import controleur
 
 # Initialisation de Pygame
 pygame.init()
 
 # Création de l'environnement et d'un véhicule
 environnement = Environnement()
+controleur = controleur.Controleur(environnement.vehicule, environnement)
 
 temps = Horloge()
 
@@ -35,36 +34,10 @@ clock = pygame.time.Clock()
 temps.demarrer()
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+
     
     # Gestion des contrôles utilisateur
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_RIGHT]:  # Augmenter la vitesse de la roue droite
-        environnement.vehicule.vit_Rd = min(150, environnement.vehicule.vit_Rd + 1)
-         
-    elif keys[pygame.K_UP]:  # Ralentir la roue droite
-        environnement.vehicule.vit_Rd = max(-50, environnement.vehicule.vit_Rd - 1)
-
-    if keys[pygame.K_LEFT]:  # Augmenter la vitesse de la roue gauche
-        environnement.vehicule.vit_Rg = min(150, environnement.vehicule.vit_Rg + 1)
-
-    elif keys[pygame.K_DOWN]:  # Ralentir la roue gauche
-        environnement.vehicule.vit_Rg = max(-50, environnement.vehicule.vit_Rg - 1)
-
-    elif keys[pygame.K_f]:
-        environnement.vehicule.freiner(0.5)
-        print("                                                       ", end ="\r")
-        print("le vehicule freine, Pschhh", end ="\r")
-
-    if keys[pygame.K_r]:
-        environnement.vehicule.restart()
-        print("                                                       ", end ="\r")
-        print("oh la la on retourne à zero", end ="\r")
-
+    controleur.gerer_evenements()
 
 
     environnement.vehicule.bouger(temps.get_temps_ecoule())
