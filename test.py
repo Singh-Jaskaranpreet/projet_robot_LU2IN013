eaf02ad265119a97ebab5f5365acb3eceee59c4c
Maxime-DeAraujo <maxime.dearaujo@outlook.fr>
2025-02-11 23:47:32 +0100

    test_initialisation tests

--- test.py
+++ test.py
@@ -11,5 +11,12 @@ class TestEnvironnement(unittest.TestCase):
         self.env = Environnement()
         self.vehicule = self.env.vehicule
 
+    def test_initialisation(self):
+        """Teste l'initialisation de l'environnement."""
+        self.assertEqual(self.env.largeur, 1200)
+        self.assertEqual(self.env.hauteur, 800)
+        self.assertIsInstance(self.env.vehicule, Vehicule)
+        self.assertEqual(len(self.env.objects), 1)  # Vérifie qu'il y a un objet par défaut
+
 if __name__ == '__main__':
     unittest.main()
