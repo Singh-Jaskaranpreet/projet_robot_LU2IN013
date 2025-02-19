4e40578e1d502238c03642b4a22e272500c28fa1
Singh-Jaskaranpreet <jassikhasriya20@gmail.com>
2025-02-19 08:57:44 +0100

    ajoute de doc en vehicule

--- src/modele/vehicule.py
+++ src/modele/vehicule.py
@@ -19,6 +19,10 @@ class Vehicule:
         self.infrarouge = Infrarouge()
     
     def accelerer(self, val):
+        """
+        Augmente la vitesse des deux roues du vehicule.
+        :param val: La vitesse augmente de val
+        """
         self.vit_Rg = max(0, min(150, self.vit_Rg+val))
         self.vit_Rd = max(0, min(150, self.vit_Rd+val))
     
@@ -33,27 +37,54 @@ class Vehicule:
             self.vit_Rd = min(0,self.vit_Rd+val)
 
     def reculer(self, val):
+        """
+        Diminue la vitesse des deux roues du vehicule.
+        :param val: La vitesse diminue de val
+        """
         self.vit_Rg = max(-50, self.vit_Rg - val)
         self.vit_Rd = max(-50, self.vit_Rd - val)
 
         
     def get_distance(self):
+        """
+        Renvoie la distance par rapport a un obstacle s'il existe.
+        """
         return self.infrarouge.mesurer_distance_obstacle(self)
     
     def set_vrd(self, x):
+        """
+        Met la roue droite a une certaine vitesse.
+        :param x: La vitesse de la roue droite est x
+        """
         self.vit_Rd = max(-150, min(x, 150))
 
     def set_vrg(self, x):
+        """
+        Met la roue gauche a une certaine vitesse.
+        :param x: La vitesse de la roue gauche est x
+        """
         self.vit_Rg = max(-150, min(x, 150))
 
     def acr_Rg(self, val):
+        """
+        Augmente la vitesse de la roue gauche.
+        :param val: La vitesse de la roue gauche augmente de val
+        """
         self.vit_Rg = max(-150,min(150,self.vit_Rg+val))
 
     def acr_Rd(self, val):
+        """
+        Augmente la vitesse de la roue droite.
+        :param val: La vitesse de la roue droite augmente de val
+        """
         self.vit_Rd = max(-150,min(150,self.vit_Rd+val))
 
     #Place les trois roues de la voiture
     def position_des_roues(self, point):
+        """
+        Renvoie la position des roues
+        :param point: Calcule de la position par rapport a point
+        """
         r_Ar = [point[0] - self.long * m.cos(m.radians(self.angle)), point[1] - self.long * m.sin(m.radians(self.angle))]
         r_Avg = [point[0] + (self.essieux // 2) * m.cos(m.radians(self.angle + 90)), point[1] + (self.essieux // 2) * m.sin(m.radians(self.angle + 90))]
         r_Avd = [point[0] + (self.essieux // 2) * m.cos(m.radians(self.angle - 90)), point[1] + (self.essieux // 2) * m.sin(m.radians(self.angle - 90))]
