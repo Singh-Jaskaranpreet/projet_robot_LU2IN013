562e01d077bcbc7204702a6b1187ac610be2b857
Tsiory-Rama <dtsiorysteven@gmail.com>
2025-03-15 23:56:43 +0100

    Robot redeviens la classe parent des adaptateurs

--- src/robot_général/robot.py
+++ src/robot_général/robot.py
@@ -1,37 +1,33 @@
 class Robot:
-    def __init__(self, vehicule, type_adaptateur):
-        # Choisir l'adaptateur en fonction du type
-        if type_adaptateur == 'RS':
-            self.adaptateur = AdaptateurRS(vehicule)
-        else:
-            self.adaptateur = AdaptateurRR(vehicule)
+    def __init__(self):
+        pass
 
-    def avancer(self, valeur):
-        self.adaptateur.avancer(valeur)
+    def avancer(self,valeur):
+        pass
 
     def arreter(self):
-        self.adaptateur.arreter()
+        pass
 
-    def v_roue_gauche(self, valeur):
-        self.adaptateur.v_roue_gauche(valeur)
+    def v_roue_gauche(self,valeur):
+        pass
 
-    def v_roue_droite(self, valeur):
-        self.adaptateur.v_roue_droite(valeur)
+    def v_roue_droite(self,valeur):
+        pass
 
-    def distance_parcouru(self, vit, temps):
-        return self.adaptateur.distance_parcouru(vit, temps)
+    def distance_parcouru(self,vit,temps):
+        pass
 
     def get_distance(self):
-        return self.adaptateur.get_distance()
+        pass
 
-    def get_temps(self, vitesse):
-        return self.adaptateur.get_temps(vitesse)
+    def get_temps(self):
+        pass
 
     def get_essieux(self):
-        return self.adaptateur.get_essieux()
+        pass
 
     def get_vitesse_Rg(self):
-        return self.adaptateur.get_vitesse_Rg()
+        pass
 
     def get_vitesse_Rd(self):
-        return self.adaptateur.get_vitesse_Rd()
+        pass
