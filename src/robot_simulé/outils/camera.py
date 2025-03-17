90df69a025a31dbf31153d06751e0304b494434a
B1gPanda <kankizilmikail.gmail.com>
2025-03-17 07:05:33 +0100

    fonction qui simule une caméra en envoyant plusieurs rayons devant le véhicule pour voir ce qu'il y a en face.

--- src/robot_simulé/outils/camera.py
+++ src/robot_simulé/outils/camera.py
@@ -11,3 +11,19 @@ class CameraView:
         self.num_rays = width        # Un rayon par colonne
         self.max_distance = 1000     # Distance maximale
         self.renderer = renderer
+
+
+    def cast_rays(self):
+        pos = self.environnement.vehicule.p_centre
+        angle = math.radians(self.environnement.vehicule.angle + self.environnement.vehicule.angle_servo)
+        start_angle = angle - self.fov / 2
+        ray_angle_step = self.fov / self.num_rays
+
+        distances = []
+        for ray in range(self.num_rays):
+            if not pygame.get_init():  # Vérifie si Pygame est toujours actif
+                return distances
+
+            ray_angle = start_angle + ray * ray_angle_step
+            distances.append(self.cast_single_ray(pos, ray_angle))
+        return distances
