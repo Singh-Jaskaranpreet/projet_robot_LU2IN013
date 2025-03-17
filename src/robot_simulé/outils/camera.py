2075c10dc209417ed3b537c135268e61d32b403d
B1gPanda <kankizilmikail.gmail.com>
2025-03-17 07:10:29 +0100

    ajout de l'affichage de la balise avec la camera

--- src/robot_simulé/outils/camera.py
+++ src/robot_simulé/outils/camera.py
@@ -79,3 +79,38 @@ class CameraView:
                 self.renderer.fill_rect(rect)
                     # Mettre à jour l'affichage
             self.renderer.present()
+
+
+    def afficher_balise(self):
+        pos = self.environnement.vehicule.p_centre
+        angle = math.radians(self.environnement.vehicule.angle + self.environnement.vehicule.angle_servo)
+        start_angle = angle - self.fov / 2
+        ray_angle_step = self.fov / self.num_rays
+
+        distances = []
+        for ray in range(self.num_rays):
+            if not pygame.get_init():  # Vérifie si Pygame est toujours actif
+                return distances
+
+            ray_angle = start_angle + ray * ray_angle_step
+            distances.append(self.cast_single_ray_b(pos, ray_angle))
+        return distances
+
+    def cast_single_ray_b(self, pos, ray_angle):
+        x, y = pos
+        for d in range(0, self.max_distance, 5):
+            test_x = x + d * math.cos(ray_angle)
+            test_y = y + d * math.sin(ray_angle)
+            
+            for obj in self.environnement.asuivre:
+                cx, cy = obj  # Centre du cercle
+                rayon = 10   # Rayon du cercle
+                    
+                distance_objet = math.sqrt((test_x - cx) ** 2 + (test_y - cy) ** 2)
+                if distance_objet <= rayon:
+                    return d  # Distance de détection du cercle
+
+            if test_x < 0 or test_x > self.environnement.largeur or test_y < 0 or test_y > self.environnement.hauteur:
+                return d
+
+        return self.max_distance
