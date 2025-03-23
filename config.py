f48f60a598e1a752b14f6cddba6d25209f3c9448
Maxime-DeAraujo <maxime.dearaujo@outlook.fr>
2025-03-23 18:37:11 +0100

    Ajout d'affichage 3D dans config.py

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+from ursina import color  # Importer color depuis ursina
+
+# Affichae 2D
 # Dimensions de la fenêtre
 LARGEUR = 1200
 HAUTEUR = 800
@@ -11,3 +14,15 @@ HAUTEUR_ROUE = 5   # Hauteur de l'ellipse pour les roues
 
 # Autres paramètres
 VITESSE_ECHELLE = 0.003
+
+
+# Affichae 3D
+# Couleurs utilisées
+COULEURS = {'sol': color.white,'vehicule': color.red,'roue': color.black,'obstacle_rect': color.magenta,'obstacle_circulaire': color.blue}
+
+# Paramètres du véhicule
+ESSIEUX = 10  # Distance entre les roues avant et arrière
+TAILLE_ROUE = 2  # Taille des roues (rayon de la sphère)
+
+# Paramètres de la caméra
+CAMERA_POSITION = (0, 50, -200)  # Position initiale de la caméra
--- src/affichage/affichage3D.py
+++ src/affichage/affichage3D.py
@@ -1,50 +1,76 @@
 from ursina import *
-
+import config
 
 
 class Affichage3D():
-    def __init__(self,environnement,controleur):
-        self.app = Ursina() # Initialisation de Ursina
+    def __init__(self, environnement, controleur):
+        self.app = Ursina()  # Initialisation de Ursina
         self.environnement = environnement
         self.voiture = environnement.vehicule
         self.v_G = self.voiture.vit_Rg
         self.v_D = self.voiture.vit_Rd
         self.controleur = controleur
-        # Création du sol                    (x,   y,  z)
-        ground = Entity(model='plane', scale=(environnement.largeur, 1, environnement.hauteur), color=color.white, collider='box',position=(environnement.largeur/2,0,environnement.hauteur/2))
 
-        #création du vehicule 3D
+        # Création du sol
+        ground = Entity(
+            model='plane', 
+            scale=(self.environnement.largeur, 1, self.environnement.hauteur),
+            color=config.COULEURS['sol'],  # Utilisation de la couleur du sol
+            collider='box',
+            position=(self.environnement.largeur / 2, 0, self.environnement.hauteur / 2)
+        )
+
+        # Création du véhicule 3D
         self.vehicule_3d = self.creer_vehicule3D()
 
-        # Création et recupération des obstacles 3D
+        # Création et récupération des obstacles 3D
         self.objets_3d = []
         self.generer_obstacles()
 
         # Position initiale de la caméra
-        camera.position = (self.voiture.p_centre[0], 50, self.voiture.p_centre[1]-200)
-        camera.look_at(self.vehicule_3d)  # Faire en sorte que la caméra regarde le robot
-        
+        camera.position = config.CAMERA_POSITION  # Utiliser la position définie dans config.py
+        camera.look_at(self.vehicule_3d)  # Faire en sorte que la caméra regarde le véhicule
+
     def creer_vehicule3D(self):
         vehicule_3d = Entity(
-        model=Mesh(vertices=[
+            model=Mesh(vertices=[
 
-        # Base(triangle isocèle)
-            Vec3(0, 0.1, -self.voiture.essieux/2), # Point 0 (roue arrière)
-            Vec3(-self.voiture.essieux/2, 0.1, self.voiture.essieux/2),  # Point 1 (roue gauche)
-            Vec3(self.voiture.essieux/2, 0.1, self.voiture.essieux/2),   # Point 2 (roue droite)
-            
-            ], 
-        # Base inférieure(permet de relier les 3 points)
+                # Base (triangle isocèle)
+                Vec3(0, 0.1, -config.ESSIEUX / 2),  # Point 0 (roue arrière)
+                Vec3(-config.ESSIEUX / 2, 0.1, config.ESSIEUX / 2),  # Point 1 (roue gauche)
+                Vec3(config.ESSIEUX / 2, 0.1, config.ESSIEUX / 2),  # Point 2 (roue droite)
+            ],
+            # Base inférieure (permet de relier les 3 points)
             triangles=[(2, 1, 0)]),
             
-        color=color.red,  # Remplissage rouge
-        position=(0, 0.1, 0)
+            color=config.COULEURS['vehicule'],  # Utiliser la couleur du véhicule
+            position=(0, 0.1, 0)
         )
-        vehicule_3d.world_position = (self.voiture.p_centre[0],1,self.voiture.p_centre[1])
-        # Roues de la voiture (en bas du prisme triangulaire)
-        roue_G = Entity(model='sphere', scale=2, position=(-self.voiture.essieux/2, 0.1, self.voiture.essieux/2), color=color.black , parent= vehicule_3d)                                                                                                                                                                                                                                                   
-        roue_D = Entity(model='sphere', scale=2, position=(self.voiture.essieux/2, 0.1, self.voiture.essieux/2), color=color.black , parent= vehicule_3d)
-        roue_Ar = Entity(model='sphere', scale=2, position=(0, 0.1, -self.voiture.essieux/2), color=color.black,parent= vehicule_3d)
+
+        vehicule_3d.world_position = (self.voiture.p_centre[0], 1, self.voiture.p_centre[1])
+
+        # Roues du véhicule (en bas du prisme triangulaire)
+        roue_G = Entity(
+            model='sphere', 
+            scale=config.TAILLE_ROUE, 
+            position=(-config.ESSIEUX / 2, 0.1, config.ESSIEUX / 2), 
+            color=config.COULEURS['roue'], 
+            parent=vehicule_3d
+        )
+        roue_D = Entity(
+            model='sphere', 
+            scale=config.TAILLE_ROUE, 
+            position=(config.ESSIEUX / 2, 0.1, config.ESSIEUX / 2), 
+            color=config.COULEURS['roue'], 
+            parent=vehicule_3d
+        )
+        roue_Ar = Entity(
+            model='sphere', 
+            scale=config.TAILLE_ROUE, 
+            position=(0, 0.1, -config.ESSIEUX / 2), 
+            color=config.COULEURS['roue'], 
+            parent=vehicule_3d
+        )
 
         return vehicule_3d
 
@@ -53,11 +79,10 @@ class Affichage3D():
         Affiche l'environnement, y compris le véhicule, les objets (obstacles),
         et la vitesse du véhicule.
         """
-
         self.controleur.gerer_evenements()
         self.environnement.bouger()
         self.environnement.rester_dans_limites()
-        self.vehicule_3d.position = (self.voiture.p_centre[0],0,self.voiture.p_centre[1])
+        self.vehicule_3d.position = (self.voiture.p_centre[0], 0, self.voiture.p_centre[1])
         self.vehicule_3d.rotation_y = self.voiture.angle
 
     def generer_obstacles(self):
@@ -69,10 +94,22 @@ class Affichage3D():
                 pos_z = (obj[0][1] + obj[2][1]) / 2
                 scale_z = abs(obj[0][0] - obj[2][0])
                 scale_x = abs(obj[0][1] - obj[2][1])
-                obstacle = Entity(model='cube', scale=(scale_x, 2, scale_z), position=(pos_x, pos_y, pos_z), color=color.magenta, collider='box')
+                obstacle = Entity(
+                    model='cube', 
+                    scale=(scale_x, 2, scale_z), 
+                    position=(pos_x, pos_y, pos_z), 
+                    color=config.COULEURS['obstacle_rect'], 
+                    collider='box'
+                )
                 self.objets_3d.append(obstacle)
             elif len(obj) == 2:  # Obstacle circulaire
                 pos_x, pos_z = obj[0]
                 rayon = obj[1]
-                obstacle = Entity(model='sphere', scale=(rayon, rayon, rayon), position=(pos_x, 0.5, pos_z), color=color.blue, collider='sphere')
+                obstacle = Entity(
+                    model='sphere', 
+                    scale=(rayon, rayon, rayon), 
+                    position=(pos_x, 0.5, pos_z), 
+                    color=config.COULEURS['obstacle_circulaire'], 
+                    collider='sphere'
+                )
                 self.objets_3d.append(obstacle)
